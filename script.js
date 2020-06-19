47ea6ed467be087d7caba4f686977eff5a294716
Bibash khadka <lama.bibash10@gmail.com>
2020-06-19 16:00:46 +0545

    yogpa site

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ $(function ()
             $('.nav-list').toggleClass('open');
         });
     });
+
+
+
+// testimonial slider
+
+$(function () {
+
+
+    $("#customers-testimonial").owlCarousel({
+
+        items: 1,
+
+        autoplay: true,
+
+        smartSpeed: 700,
+
+        loop: true,
+
+        autoPlayHoverPause: true
+
+
+
+
+    });
+});
